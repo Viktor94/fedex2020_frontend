02adc10342d9ea7be050d081243d541f55ecba87
Divad001 <gyoredavid1993@gmail.com>
2020-09-23 14:05:32 +0200

    added heroku URL

--- login_script.js
+++ login_script.js
@@ -1,19 +1,23 @@
 'use strict';
 
-const URL = 'http://localhost:8080/';
+//const URL = 'http://localhost:8080/'
+const URL = 'https://cors-anywhere.herokuapp.com/https://fedex-backend.herokuapp.com/';
 const form = document.querySelector('form');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
   const username = document.getElementById('username').value;
   const pwd = document.getElementById('password').value;
-  const login = { username: username, pwd: pwd };
+  const login = { username: username, password: pwd };
   fetch(URL + 'login', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json',
+      'Content-Type': 'application/json'
     },
     body: JSON.stringify(login)
+  })
+  .then((response) => {
+    console.log(response)
   });
   form.reset();
   // window.location = URL + 'index.html';
